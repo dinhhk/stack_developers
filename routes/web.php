924572fcdae395b39c3fedfd5569c8f3d18f36e5
dinhhk <hankimdinh@gmail.com>
2019-01-15 21:29:46 +0700

    [master] Implement login page and handle login process

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::match(['get', 'post'], '/admin', 'AdminController@login')->name('admin');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
